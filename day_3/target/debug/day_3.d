192d5f14bc8f661013fea47c8345ebd96f641442
tim <tim7voetbal@live.nl>
2025-12-04 19:17:23 +0100

    day3

--- day_3/target/debug/day_3.d
+++
@@ -1,1 +0,0 @@
-/home/theeem/Projects/AdventOfCode2025/day_3/target/debug/day_3: /home/theeem/Projects/AdventOfCode2025/day_3/src/main.rs
--- day_3/target/debug/deps/day_3-0dee44b3f2c2d372.d
+++
@@ -1,5 +0,0 @@
-/home/theeem/Projects/AdventOfCode2025/day_3/target/debug/deps/libday_3-0dee44b3f2c2d372.rmeta: src/main.rs
-
-/home/theeem/Projects/AdventOfCode2025/day_3/target/debug/deps/day_3-0dee44b3f2c2d372.d: src/main.rs
-
-src/main.rs:
